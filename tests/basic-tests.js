ab305333a5219f1645d77ec7d8cbba3b6d9046c0
Chris Tavares <ctavares@microsoft.com>
2015-09-03 16:42:59 -0700

    Fixed up validation, testing validation failures

--- tests/basic-tests.js
+++ tests/basic-tests.js
@@ -5,6 +5,7 @@ var joe = require('joe');
 var tv4 = require('tv4');
 
 var schema = require('swagger-schema-official/schema.json');
+tv4.addSchema(schema);
 
 joe.describe('Valid swagger', function (describe, it) {
   var swagger = require('./sample-swagger/basicValid.swagger.json');
@@ -12,5 +13,14 @@ joe.describe('Valid swagger', function (describe, it) {
   it('should be valid', function () {
     var result = tv4.validateResult(swagger, schema);
     expect(result.valid).to.be.true;
+  });
+});
+
+joe.describe('Invalid swagger', function (describe, it) {
+  var swagger = require('./sample-swagger/missingParameterTypes.swagger.json');
+
+  it('should not be valid', function () {
+    var result = tv4.validateMultiple(swagger, schema);
+    expect(result.valid).to.be.false;
   });
 });
