13d69d083d8b4d2304887b8148e49398d95b100b
Chris Tavares <ctavares@microsoft.com>
2015-09-11 17:23:17 -0700

    Hooking up handlebars engine

--- app/index.js
+++ app/index.js
@@ -1,11 +1,28 @@
 'use strict';
 
 var Hapi = require('hapi');
+var Hoek = require('hoek');
 
 var server = new Hapi.Server();
 
+server.reg
+
 var port = +(process.env['PORT'] || 3000);
 server.connection({port: port});
+server.register(require('vision'), function (err) {
+  Hoek.assert(!err, err);
+
+  server.views({
+    engines: {
+      hbs: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './views',
+    layoutPath: './views/layouts',
+    partialsPath: './views/partials',
+    helpersPath: './lib/viewHelpers'
+  })
+});
 
 server.route({
   method: 'GET',
