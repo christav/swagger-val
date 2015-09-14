73d2957c3aeef1c9bbde846ade120dc54fcee5d3
Chris Tavares <ctavares@microsoft.com>
2015-09-14 10:29:18 -0700

    Refactored route registration, brought in static file handling.
    Also brought in bower & bootstrap.

--- app/index.js
+++ app/index.js
@@ -1,15 +1,19 @@
 'use strict';
 
+var debug = require('debug')('swagger-val:main');
 var Hapi = require('hapi');
 var Hoek = require('hoek');
 
+var sfmt = require('sfmt');
+
+var routes = require('./routes');
 var server = new Hapi.Server();
 
 server.reg
 
 var port = +(process.env['PORT'] || 3000);
 server.connection({port: port});
-server.register(require('vision'), function (err) {
+server.register([require('vision'), require('inert')], function (err) {
   Hoek.assert(!err, err);
 
   server.views({
@@ -21,15 +25,13 @@ server.register(require('vision'), function (err) {
     layoutPath: './views/layouts',
     partialsPath: './views/partials',
     helpersPath: './lib/viewHelpers'
-  })
-});
+  });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: {
-    view: 'index'
-  }
+
+  routes.forEach(function (route) {
+    debug(sfmt('Binding route for %{0}', route.path));
+    server.route(route);
+  });
 });
 
 module.exports = server;
