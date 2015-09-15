a5a38d671ca0c8e583d5dd9d0a8bf6d81ff5385e
Chris Tavares <ctavares@microsoft.com>
2015-09-15 12:47:10 -0700

    Removed leftover logging

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -4,16 +4,14 @@ $(function () {
   var inputOptions = [
     [ $('input[type="radio"][value="input"]'), $('#swaggerInput') ],
     [ $('input[type="radio"][value="url"]'), $('#swaggerUrl') ],
-    [ $('input[type="radio"][value="file"]'), $('#swaggerFile')]
+    [ $('input[type="radio"][value="file"]'), $('#swaggerFile') ]
   ];
 
   function updateVisibility() {
     inputOptions.forEach(function (pair) {
       if (!pair[0][0].checked) {
-        console.log('input valued ' + pair[0].attr('value') + ' is not checked, hiding');
         pair[1].hide();
       } else {
-        console.log('input valued ' + pair[0].attr('value') + ' is checked, showing');
         pair[1].show();
       }
     });
